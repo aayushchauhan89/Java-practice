640bfa4014d43b7eb38338456342b0562cb204cb
Aayush Chauhan <aayushchauhan89419@gmail.com>
2026-06-21 23:23:24 +0530

    bitwiseAndOR

---
+++ BitwiseOper.java
@@ -0,0 +1,9 @@
+public class BitwiseOper{
+    public static void main(String [] args){
+        System.out.println(108&31);
+        System.out.println(108|31);
+        System.out.println(0^6);
+        System.out.println(3^3);
+        System.out.println(~4);
+    }
+}
